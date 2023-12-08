a19bfbd853914ed3b1667e204028ae06568178b2
Van Trinh <vantrinh@berkeley.edu>
2023-12-07 22:40:00 -0800

    initial work on proj

---
+++ new_tests/test_inventory_manager.py
@@ -0,0 +1,12 @@
+import unittest
+from inventory import Inventory, Box, Sample, Location, Culture, Concentration
+from inventory_manager.inventory_manager import InventoryManager
+
+
+class TestInventoryManager(unittest.TestCase):
+    def test_inventory(self):
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
